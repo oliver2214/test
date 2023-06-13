9707f857ef83819219010b1d7e25632363fe3439
oliver2214 <dima.tovstokor@yandex.ru>
2023-06-13 09:20:39 +0300

    change2

--- draft.py
+++ draft.py
@@ -4,8 +4,7 @@ student_names = ['Camila Rodriguez', 'Juan Cruz', 'Dan Richards', 'Sam Boyle', '
 student_grades = [86, 98, 89, 92, 45, 67, 89, 90, 100, 98, 10, 96, 93]
 
 result = [{i:{j:k}} for i, j, k in zip(student_ids, student_names, student_grades)]
-print(result)
+print(result[1])
 
-for _ in range(5):
-    print(1)
-
+for i in range(len(result)):
+    print(result[i])
